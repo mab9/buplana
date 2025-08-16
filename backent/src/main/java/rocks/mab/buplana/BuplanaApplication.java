843deb55caef959e311df8890fae39c06e542e43
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2025-08-16 10:11:49 +0200

    Create backent mvn module

---
+++ backent/src/main/java/rocks/mab/buplana/BuplanaApplication.java
@@ -0,0 +1,13 @@
+package rocks.mab.buplana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuplanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuplanaApplication.class, args);
+	}
+
+}
